51afabd997c67fd1f98f82c7a3ccf399ad82630a
Antonio Giroz <antonioluisgil@gmail.com>
2023-11-21 07:46:01 +0100

    Initial commit

---
+++ module01/kata01-testing-library/src/testLib.ts
@@ -0,0 +1,26 @@
+export function expect<T>(expected: T) {
+  return {
+    toBe(actual: T) {
+      if (actual !== expected) {
+        throw new Error(`\tExpected ${expected}, but got ${actual}`);
+      }
+    }
+  }
+}
+
+export async function test(description: string, callback: () => void) {
+  try {
+    await callback();
+    console.log(`✅ ${description}`); 
+  } catch (error) {
+    console.log(`❌ ${description}`);
+    console.log(error.message);
+  }    
+}
+
+export const it = test;
+
+export function describe(description: string, callback: () => void) {
+  console.log(description);
+  callback();
+}
